fe24d2d4d7d5167da08f6b8612a3425788b61fb2
Esen Biakhmet <kaltayev17@gmail.com>
2025-04-19 23:13:56 +0500

    fix cors

--- index.js.js
+++ index.js.js
@@ -11,6 +11,8 @@ const CHAT_ID = process.env.CHAT_ID;
 const WEB_APP_URL = process.env.WEB_APP_URL;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}/test`;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -106,5 +108,3 @@ app.post('/mark-done', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`✅ Server running at http://localhost:${PORT}`);
 });
-
-app.use(cors());
